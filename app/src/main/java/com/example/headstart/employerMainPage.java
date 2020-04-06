6c661433376b00b144427d83f5f4034dce11aec5
jeffr <jtwilcox@wpi.edu>
2020-04-06 14:49:13 -0400

    !?!Gamers!?!

--- app/src/main/java/com/example/headstart/employerMainPage.java
+++ app/src/main/java/com/example/headstart/employerMainPage.java
@@ -2,7 +2,9 @@ package com.example.headstart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class employerMainPage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class employerMainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_main_page);
+    }
+
+    public void performOpenNewJob(View v){
+        Intent intent = new Intent(this, NewJob.class);
+        startActivity(intent);
     }
 }
