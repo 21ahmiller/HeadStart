c51265982d19ec632b484f93ff5f6610fe65725a
jeffr <jtwilcox@wpi.edu>
2020-04-09 13:42:47 -0400

    !?!Gamers!?!

--- app/src/main/java/com/example/headstart/NewJob.java
+++ app/src/main/java/com/example/headstart/NewJob.java
@@ -2,7 +2,10 @@ package com.example.headstart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewJob extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class NewJob extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_job);
+    }
+
+    public void onClickSubmit(View v){
+        EditText jobNameText = findViewById(R.id.jobNameText);
+        String jobTitle = jobNameText.getText().toString();
+
+        EditText jobInformationText = findViewById(R.id.jobInformationText);
+        String jobInformation = jobInformationText.getText().toString();
+
+        EditText locationText = findViewById(R.id.locationText);
+        String location = locationText.getText().toString();
+
+        EditText requirementsText = findViewById(R.id.requirementsText);
+        String requirements = requirementsText.getText().toString();
+
+        EditText preferredSkillsText = findViewById(R.id.preferredSkillsText);
+        String preferredSkills = preferredSkillsText.getText().toString();
+
+        EditText timeText = findViewById(R.id.timeText);
+        String time = timeText.getText().toString();
+
+        EditText salaryText = findViewById(R.id.salaryText);
+        String salary = salaryText.getText().toString();
+
+        EditText benefitsText = findViewById(R.id.benefitsText);
+        String benefits = benefitsText.getText().toString();
+
+        Intent intent = new Intent(this, employerMainPage.class);
+        startActivity(intent);
     }
 }
