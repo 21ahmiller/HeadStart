73b662f9c8751a4085e1403c1a5e20827401b120
jeffr <jtwilcox@wpi.edu>
2020-04-21 14:20:54 -0400

    !?!Gamers!?!

--- app/src/main/java/com/example/headstart/CreateEmployer.java
+++ app/src/main/java/com/example/headstart/CreateEmployer.java
@@ -2,7 +2,10 @@ package com.example.headstart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateEmployer extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class CreateEmployer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_employer);
+    }
+
+    public void performCreateEmployer(View v){
+        EditText emailText = findViewById(R.id.companyEmailText);
+        String email = emailText.getText().toString();
+
+        EditText passwordText = findViewById(R.id.companyPasswordText);
+        String password = passwordText.getText().toString();
+
+        EditText usernameText = findViewById(R.id.companyNameText);
+        String displayName = usernameText.getText().toString();
+
+        User applicant = new User(email, password, displayName);
+        Intent intent = new Intent(this, employerMainPage.class);
+        startActivity(intent);
     }
 }
