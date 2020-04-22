c914f2eb266704ee8ff1ae94cd3ed86df8ecb09f
matthewyoung <mryoung@wpi.edu>
2020-04-21 22:43:44 -0400

    Java files

---
+++ app/src/main/java/com/example/headstart/Master.java
@@ -0,0 +1,13 @@
+package com.example.headstart;
+
+public class Master {
+    public static void main(String args[]){
+
+    }
+
+    public User user;
+
+    public static void getUser(){
+
+    }
+}
